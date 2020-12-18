68581645445f6735694bb6ce533297e11da3b21e
MykolaPMP <mkrainyk26@gmail.com>
2020-12-18 23:38:58 +0200

    Adding views for details, list, delete for statements

---
+++ Statement/ViewModel/StatementWithAllStatuses.cs
@@ -0,0 +1,15 @@
+﻿using BusinessTrip.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Statement.ViewModel
+{
+    public class StatementWithAllStatuses
+    {
+        public ApplicationStatement Statement { get; set; }
+        public ApplicationCurrentStatus CurrentStatus { get; set; }
+        public List<ApplicationHistoryOfStatus> HistoryOfStatuses { get; set; }
+    }
+}
